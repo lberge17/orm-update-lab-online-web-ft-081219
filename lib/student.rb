54d2a0699bee3c8062b7cde37635bcceef4d747d
Laura Berge <lauraeberge@gmail.com>
2019-09-21 21:40:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,13 +30,13 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def save(name, grade)
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
       SQL
     
-    DB[:conn].execute(sql, name, grade)
+    DB[:conn].execute(sql, self.name, self.grade)
     
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
